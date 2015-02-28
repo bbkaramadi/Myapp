73c8937760a969fa96854d33a6f841e7cf1b65d2
bbkaramadi <bbkaramadi@gmail.com>
2015-02-28 23:47:57 +0530

    first commit

---
+++ app/models/Employee.java
@@ -0,0 +1,11 @@
+package models;
+
+
+public class Employee {
+
+	public int employeeId;
+	public String employeeName;
+	public int salary;
+	public String department;
+	
+}
---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "myApp"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
